370739390ea721257a552b99b4b2f658585b3fa8
LuisDelgado652 <luisdelgadobravo652@gamil.com>
2023-07-09 17:03:57 -0500

    new

--- src/models/examen.js
+++ src/models/examen.js
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 const ExamenSchema = new Schema({
   descripcion: { type: String },
   indicacion: { type: String },
+  enfermedad: {type: String},
 });
 
 module.exports = mongoose.model("Examenes", ExamenSchema);
