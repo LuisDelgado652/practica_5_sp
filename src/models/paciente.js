d2e7bac08f57b6dd5c8c8ed4345c3f6fea1ca2f4
LuisDelgado652 <luisdelgadobravo652@gamil.com>
2023-07-09 17:24:43 -0500

    Cambio modelo paciente

--- src/models/paciente.js
+++ src/models/paciente.js
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 const PacienteSchema = new Schema({
   nombre: { type: String },
   identificacion: { type: String },
+  edad: { type: String },
 });
 
 module.exports = mongoose.model("Pacientes", PacienteSchema);
