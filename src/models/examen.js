1ce0b158b5ab0336092c2ac599ba59dde6ca5662
LuisDelgado652 <luisdelgadobravo652@gamil.com>
2023-07-10 10:36:32 -0500

    prueba en clase

--- src/models/examen.js
+++ src/models/examen.js
@@ -4,7 +4,6 @@ const { Schema } = mongoose;
 const ExamenSchema = new Schema({
   descripcion: { type: String },
   indicacion: { type: String },
-  enfermedad: {type: String},
 });
 
 module.exports = mongoose.model("Examenes", ExamenSchema);
